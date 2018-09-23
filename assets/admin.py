b23e6b4787f994c2ac6167750b1b0921843962a5
parvezmullah <rocksparvezmulla1@gmail.com>
2018-09-23 18:02:02 +0530

    Asset management done

--- assets/admin.py
+++ assets/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
 from .models import(
-    Assets
+    Assets,
+    AssetPurchase,
 )
 # Register your models here.
+class AssetPurchaseAdmin(admin.ModelAdmin):
+    list_display = [ 'asset_name', 'asset_quantity','total_price', 'date_purchased']
+    list_per_page = 10
+    
+    class Meta:
+        model = AssetPurchase
+
+admin.site.register(AssetPurchase, AssetPurchaseAdmin)
 admin.site.register(Assets)
--- assets/models.py
+++ assets/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Assets(models.Model):
-    asset_name = models.CharField(max_length=30)
+    asset_name = models.CharField(max_length=30, primary_key=True)
 
     def __str__(self):
         if self.asset_name==None:
             return "ERROR-CUSTOMER NAME IS NULL"
+        return self.asset_name
+
+    class Meta:
+        verbose_name_plural = "assets"
+
+class AssetPurchase(models.Model):
+    asset_name = models.OneToOneField(Assets, on_delete=models.CASCADE,)
+    asset_quantity = models.IntegerField()
+    date_purchased = models.DateField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    total_price = models.IntegerField(help_text='In Thousands')
+
+    def __str__(self):
         return self.asset_name
